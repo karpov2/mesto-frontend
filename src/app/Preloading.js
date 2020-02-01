b3812dba3b725029bfff6aa45eb5e551b6a2cb88
Ilya Karpov <karpov.business@gmail.com>
2020-02-02 00:10:23 +0500

    delete

--- src/app/Preloading.js
+++
@@ -1,9 +0,0 @@
-export class Preloading {
-    constructor(dom) {
-        this.visible = dom.visible;
-    }
-
-    load(node) {
-        node.firstElementChild.classList.toggle(this.visible);
-    }
-}
