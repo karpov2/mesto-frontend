76e5627cff47bcff2f4a0629061787892260443b
Ilya Karpov <karpov.business@gmail.com>
2020-02-03 15:04:33 +0500

    Вынес targets в отдельный файл

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,6 @@ const presets = [
     [
         "@babel/preset-env",
         {
-            targets: { // указать цели, для полифилов
-                edge: "17",
-                ie: "11",
-                firefox: "50",
-                chrome: "64",
-                safari: "11.1",
-            },
             useBuiltIns: "usage", // эта настройка babel-polyfill, если стоит значение usage, то будут подставлятся полифилы для версий браузеров которые указали ниже.
             corejs: "3.4.1", // явно проставить версию corejs
             modules: false,
