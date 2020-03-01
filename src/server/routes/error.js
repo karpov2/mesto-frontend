ad70b0bf70b37a6f958a4e582863039ac7515c3c
Ilya Karpov <karpov.business@gmail.com>
2020-03-01 12:44:45 +0500

    Добавил валидацию, и дополнительное 9-е задание

--- src/server/routes/error.js
+++
@@ -1,7 +0,0 @@
-const errorPage = (req, res) => {
-    res.set('Content-Type', 'application/json')
-       .status(404)
-       .send({ "message": "Запрашиваемый ресурс не найден" });
-};
-
-module.exports = errorPage;
