f95b1eb4c6f8e0421a084601bd679724be02e392
Ilya Karpov <karpov.business@gmail.com>
2020-04-05 17:46:12 +0500

    переименовал

--- src/server/middleware/authorization.js
+++
@@ -1,6 +0,0 @@
-const authorization = (req, res, next) => {
-    req.user = {_id: '5e5a98e279c940480c66a2d7'};
-    next();
-};
-
-module.exports = authorization;
--- src/server/middleware/error.js
+++
@@ -1,6 +0,0 @@
-const error = (error, req, res, next) => {
-    if (error) return res.status(400).json(error);
-    next();
-};
-
-module.exports = error;
